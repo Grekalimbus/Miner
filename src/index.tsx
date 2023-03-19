3284b4a11412d596ff57f4d750435d6bf56a42c1
Grechkin Danil <grechkin-danil@mail.ru>
2023-03-19 14:42:36 +0200

    fix styles

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      {' '}
       <App />
     </Provider>
   </BrowserRouter>
